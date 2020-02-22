bced2f1d57a88e39697fa4e0b45b5430ee373162
Mozuku <mozow4780@gmail.com>
2020-02-22 21:32:48 +0900

    [ADD] 回答フォームを作成v2

--- src/pages/admin/Admin.vue
+++ src/pages/admin/Admin.vue
@@ -1,34 +1,108 @@
 <template>
-    <div class="members">
-        <div class="user-card">
-            <div class="user-upper-contents">
-                <div class="user-icon">
-                    <i class="fas fa-user"></i>
+    <div>
+        <Modal :show="resetPasswordForm.modal" title="パスワードをリセット">
+            <sui-form>
+                <sui-form-fields fields="two">
+                    <sui-form-field>
+                        <input placeholder="From ..." />
+                    </sui-form-field>
+                    <sui-form-field>
+                        <input placeholder="To" />
+                    </sui-form-field>
+                </sui-form-fields>
+                <div class="space h30"></div>
+                <sui-button @click="resetPasswordForm.modal = false">キャンセル</sui-button>
+                <sui-button type="submit">変更</sui-button>
+            </sui-form>
+        </Modal>
+        <Modal :show="editBasicInfoForm.modal" title="基本情報を変更">
+            <sui-form>
+                <sui-form-field>
+                    <label>Name</label>
+                    <sui-form-fields fields="two">
+                        <sui-form-field>
+                            <input
+                                    type="text"
+                                    name="name_first"
+                                    placeholder="氏名"
+                            />
+                        </sui-form-field>
+                        <sui-form-field>
+                            <input
+                                    type="text"
+                                    name="name-last"
+                                    placeholder="名前"
+                            />
+                        </sui-form-field>
+                    </sui-form-fields>
+                </sui-form-field>
+                <sui-form-field>
+                    <label>システム情報</label>
+                    <sui-form-fields>
+                        <sui-form-field width="twelve">
+                            <input
+                                    type="text"
+                                    name="login_id"
+                                    placeholder="ログイン固有ID"
+                            />
+                        </sui-form-field>
+                        <sui-form-field width="four">
+                            <input type="text" name="school-yars" placeholder="学年" />
+                        </sui-form-field>
+                    </sui-form-fields>
+                </sui-form-field>
+                <div class="space h30"></div>
+                <sui-button @click="editBasicInfoForm.modal = false">キャンセル</sui-button>
+                <sui-button type="submit">変更</sui-button>
+            </sui-form>
+        </Modal>
+        <div class="members">
+            <div v-for="i in 10" :key="i" class="user-card">
+                <div class="user-upper-contents">
+                    <div class="user-icon">
+                        <i class="fas fa-user"></i>
+                    </div>
+                    <div class="user-information">
+                        <h2>Riku Mochizuki</h2>
+                        <p>riku_mochizuki -  20日前に参加</p>
+                    </div>
                 </div>
-                <div class="user-information">
-                    <h2>Riku Mochizuki</h2>
-                    <p>riku_M @ </p>
+                <div class="user-lower-contents">
+                    <sui-dropdown text="操作">
+                        <sui-dropdown-menu>
+                            <sui-dropdown-item>プロフィールを表示</sui-dropdown-item>
+                            <sui-dropdown-item @click="editBasicInfoForm.modal = true">基本情報を変更</sui-dropdown-item>
+                            <sui-dropdown-item @click="resetPasswordForm.modal = true">パスワードを変更</sui-dropdown-item>
+                            <sui-dropdown-item>管理権限を付与
+                                <span class="description">すべてのデータにアクセス可能</span>
+                            </sui-dropdown-item>
+                        </sui-dropdown-menu>
+                    </sui-dropdown>
                 </div>
-            </div>
-            <div class="user-lower-contents">
-                <sui-dropdown text="操作">
-                    <sui-dropdown-menu>
-                        <sui-dropdown-item>プロフィールを表示</sui-dropdown-item>
-                        <sui-dropdown-item>基本情報を変更</sui-dropdown-item>
-                        <sui-dropdown-item>パスワードを変更</sui-dropdown-item>
-                        <sui-dropdown-item>管理権限を付与
-                            <span class="description">すべてのデータにアクセス可能</span>
-                        </sui-dropdown-item>
-                    </sui-dropdown-menu>
-                </sui-dropdown>
             </div>
         </div>
     </div>
 </template>
 
 <script>
+    import Modal from '@/components/Modal';
+
     export default {
-        name: "Admin"
+        name: "Admin",
+        components: {
+            Modal,
+        },
+        data() {
+            return {
+                resetPasswordForm: {
+                    modal: false,
+                },
+
+                editBasicInfoForm: {
+                    modal: false,
+                }
+            }
+        }
     }
 </script>
 
@@ -38,6 +112,10 @@
         display: flex;
         align-items: center;
         justify-content: space-between;
+
+        &:nth-child(n + 1) {
+            border-bottom: 1px solid #e8e8e8;
+        }
 
         .user-upper-contents {
             display: flex;
@@ -53,6 +131,15 @@
 
             .user-information {
                 padding: 0 0 0 30px;
+                color: $default-link-color;
+
+                h2 {
+                    margin: 0 0 5px 0;
+                }
+
+                p {
+                    font-size: 13px;
+                }
             }
         }
 
