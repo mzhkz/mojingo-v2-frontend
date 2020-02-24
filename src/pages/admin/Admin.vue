fbdfcfbb0502da73c8cc0a516d77dad9a39a17cb
Mozuku <mozow4780@gmail.com>
2020-02-25 00:13:33 +0900

    [ADD] フロントエンドが完成v2

--- src/pages/admin/Admin.vue
+++ src/pages/admin/Admin.vue
@@ -1,5 +1,56 @@
 <template>
     <div>
+        <Modal :show="registerUserForm.modal" title="ユーザーを追加">
+            <sui-form>
+                <sui-form-field>
+                    <label>Name</label>
+                    <sui-form-fields fields="two">
+                        <sui-form-field>
+                            <input
+                                    type="text"
+                                    name="name_first"
+                                    placeholder="氏名"
+                            />
+                        </sui-form-field>
+                        <sui-form-field>
+                            <input
+                                    type="text"
+                                    name="name-last"
+                                    placeholder="名前"
+                            />
+                        </sui-form-field>
+                    </sui-form-fields>
+                </sui-form-field>
+                <sui-form-field>
+                    <label>システム情報</label>
+                    <sui-form-fields>
+                        <sui-form-field width="twelve">
+                            <input
+                                    type="text"
+                                    name="login_id"
+                                    placeholder="ログイン固有ID"
+                            />
+                        </sui-form-field>
+                        <sui-form-field width="four">
+                            <input type="text" name="school-yars" placeholder="学年" />
+                        </sui-form-field>
+                    </sui-form-fields>
+                </sui-form-field>
+                <sui-form-field>
+                    <label>権限</label>
+                    <sui-dropdown
+                            placeholder="権限"
+                            selection
+                            :options="setPermissionForm.options"
+                            v-model="setPermissionForm.current"></sui-dropdown>
+                </sui-form-field>
+                <div class="space h30"></div>
+                <sui-button @click="editBasicInfoForm.modal = false">キャンセル</sui-button>
+                <sui-button type="submit">作成</sui-button>
+            </sui-form>
+        </Modal>
+
+
         <Modal :show="setPermissionForm.modal" title="権限を変更">
             <sui-form-field>
                 <label>権限</label>
@@ -66,7 +117,12 @@
                 <sui-button type="submit">変更</sui-button>
             </sui-form>
         </Modal>
+
+
         <div class="members">
+            <div class="register-user" @click="registerUserForm.modal = true">
+                <h2>ユーザーを追加</h2>
+            </div>
             <div v-for="i in 10" :key="i" class="user-card">
                 <div class="user-upper-contents">
                     <div class="user-icon">
@@ -97,6 +153,21 @@
 <script>
     import Modal from '@/components/Modal';
 
+    const parmissions = [
+        {
+            text: '生徒',
+            value: 1,
+        },
+        {
+            text: '管理者',
+            value: 2,
+        },
+        {
+            text: '利用制限（ログイン不可）',
+            value: 0,
+        },
+    ];
+
     export default {
         name: "Admin",
         components: {
@@ -115,20 +186,12 @@
                 setPermissionForm: {
                     modal: false,
                     current: 1,
-                    options: [
-                        {
-                            text: '生徒',
-                            value: 1,
-                        },
-                        {
-                            text: '管理者',
-                            value: 2,
-                        },
-                        {
-                            text: '利用制限（ログイン不可）',
-                            value: 0,
-                        },
-                    ],
+                    options: Object.assign([], parmissions),
+                },
+
+                registerUserForm: {
+                    modal: false,
+                    options: Object.assign([], parmissions),
                 }
             }
         }
@@ -136,6 +199,22 @@
 </script>
 
 <style scoped lang="scss">
+
+    .register-user {
+        padding: 20px;
+        text-align: center;
+        cursor: pointer;
+        border: 2px dotted #e2e2e2;
+        margin: 10px 0;
+
+        transition: 200ms;
+
+        &:hover {
+            background: $app-primary-focus-color;
+            border: 2px dotted $default-link-color;
+        }
+    }
+
     .user-card {
         padding: 20px;
         display: flex;
